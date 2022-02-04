5e0ea5b6bff88f971148b0be57722cda79f5bd9b
hotsauceuniverse <jangsy6976@naver.com>
2022-02-04 23:19:03 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React from "react"
 import { useState, useEffect } from "react";
 import title_logo from "./img/React.png"
 import "./App.css"
 import axios from "axios";
 
-
 function App() {
   axios({
     method:'get',
-    url: 'https://api.bithumb.com/public/ticker/ALL'
+    url: "https://api.coinpaprika.com/v1/tickers"
   }).then(function (response) {
     console.log(response)
   })
@@ -23,6 +23,10 @@ function App() {
     })
   }, [])
 
+  
+
+
+
   return (
     <>
       <div className="header">
@@ -31,15 +35,34 @@ function App() {
       </div>
       <div>  
         {loading ? (
-          <p>Loading...</p>
+          <p className="loading_comment">Loading...</p>
           ) : (
-          <select>
-            {coins.map((coin) => ( 
-              <option>
-                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
+          <>
+            <select>
+                {coins.map((coin) => (
+                  <option>
+                    {coin.name} ({coin.symbol})
+                  </option>
+                ))}
+            </select>
+            {/* beta_value, circulating_supply, first_data_at, id, last_updated, max_supply, name, rank, symbol, total_supply */}
+            <ul className="coin_list">
+              {coins.map((coins) => 
+                <>
+                  <li>name: {coins.name}</li>
+                  <li>id: {coins.id}</li>
+                  <li>symbol: {coins.symbol}</li>
+                  <li>rank: {coins.rank}</li>
+                  <li>beta_value: {coins.beta_value}</li>
+                  <li>circulating_supply: {coins.circulating_supply}</li>
+                  <li>first_data_at: {coins.first_data_at}</li>
+                  <li>last_updated: {coins.last_updated}</li>
+                  <li>max_supply: {coins.max_supply}</li>
+                  <li>total_supply: {coins.total_supply}</li>
+                </>
+              )}
+            </ul>
+          </>
         )}
       </div>
     </>  
